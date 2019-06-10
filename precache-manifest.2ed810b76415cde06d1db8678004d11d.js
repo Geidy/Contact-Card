801b83c4d83b733cd256612c9145d5c3ad84a82f
Geidy <geidydm@gmail.com>
2019-06-10 08:55:47 -0400

    Updates

---
+++ precache-manifest.2ed810b76415cde06d1db8678004d11d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e598b30e7f1d220f6d8ae9a47366c6",
+    "url": "/Contact-Card/index.html"
+  },
+  {
+    "revision": "128f1b37e97fc4b1e960",
+    "url": "/Contact-Card/static/css/main.5dd250bc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Contact-Card/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "128f1b37e97fc4b1e960",
+    "url": "/Contact-Card/static/js/main.defef04a.chunk.js"
+  },
+  {
+    "revision": "313e79efaebaeb17ba80",
+    "url": "/Contact-Card/static/js/runtime~main.c50565d9.js"
+  },
+  {
+    "revision": "ab6407f2416b0d5a785706ec5653643f",
+    "url": "/Contact-Card/static/media/santorini2.ab6407f2.jpg"
+  }
+]);
